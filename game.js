0f18c0db186039e1f99355c053ed63d3cff6b244
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-07-23 22:29:45 +0200

    Refactor code to work in DOM

--- game.js
+++ game.js
@@ -5,36 +5,115 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+
 if (playerSelection == computerSelection) {
     return ("You draw")
 } else if (playerSelection == "paper") {
     if (computerSelection == "rock") {
-        return ("You Win!")
+        playerScore ++
+        return ("You win!")
     } else {
+        computerScore ++
         return ("You lose!")
     }
 } else if (playerSelection == "rock") {
     if (computerSelection == "scissors") {
+        playerScore ++
         return ("You win!")
     } else {
+        computerScore ++
         return ("You lose!")
     }
 } else if (playerSelection == "scissors") {
     if (computerSelection == "paper") {
+        playerScore ++
         return("You win!") 
     } else {
+        computerScore ++
         return ("You lose!")
     }
 
 } 
 }
 
-
-
-for (let i = 0; i < 5; i++) {
-const playerSelection = prompt("Rock, paper or scissors?").toLocaleLowerCase();
-const computerSelection = computerPlay().toLowerCase();
-console.log("You picked: " + playerSelection)
-console.log("Computer picked: " + computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+function createResult() {
+    const newP = document.createElement('p');
+    newP.classList.add('newP');
+    newP.textContent = playRound(playerSelection, computerSelection);
+    resultsContainer.appendChild(newP);
+    checkIfWon()
 }
+
+function gameReset(){
+    computerScore = 0;
+    playerScore = 0;
+    removeAllChildNodes(resultsContainer)
+    resultsContainer.appendChild(overallScore);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function checkIfWon() {
+    if (playerScore === 5) {
+        console.log("Player won!")
+        gameReset()
+
+    } else if (computerScore === 5) {
+        console.log("Computer won!")
+        gameReset()
+    }
+}
+
+let computerScore = 0;
+let playerScore = 0;
+
+let computerSelection = computerPlay().toLowerCase();
+
+const resultsContainer = document.querySelector('#results')
+
+const overallScore = document.createElement('p');
+overallScore.classList.add('overallScore')
+overallScore.textContent = 'Score: ' + playerScore + '/' + computerScore;
+resultsContainer.appendChild(overallScore);
+
+const rBtn = document.querySelector('#rock')
+
+rBtn.addEventListener('click', () => {
+    playerSelection = 'rock'
+    createResult();
+    computerPlay();
+    computerSelection = computerPlay().toLowerCase()
+    overallScore.textContent = 'Score: ' + playerScore + '/' + computerScore;
+});
+
+const pBtn = document.querySelector('#paper')
+
+pBtn.addEventListener('click', () => {
+    playerSelection = 'paper'
+    createResult();
+    computerPlay();
+    computerSelection = computerPlay().toLowerCase()
+    overallScore.textContent = 'Score: ' + playerScore + '/' + computerScore;
+});
+
+const sBtn = document.querySelector('#scissors')
+
+sBtn.addEventListener('click', () => {
+    playerSelection = 'scissors'
+    createResult();
+    computerPlay();
+    computerSelection = computerPlay().toLowerCase()
+    overallScore.textContent = 'Score: ' + playerScore + '/' + computerScore;
+});
+
+
+
+
+
+
+
+
